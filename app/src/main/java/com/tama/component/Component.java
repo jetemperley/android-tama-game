ded829b7cacef9b495dfeffc3c6eb0559bfe5c33
Jacob Temperley <jetemperley@gmail.com>
2023-12-29 18:42:40 +1100

    This needs to be serializable.

--- app/src/main/java/com/tama/component/Component.java
+++ app/src/main/java/com/tama/component/Component.java
@@ -2,7 +2,7 @@ package com.tama.component;
 
 import com.tama.thing.Thing;
 
-public abstract class Component
+public abstract class Component implements java.io.Serializable
 {
     Thing parent;
     public Component(Thing parent)
